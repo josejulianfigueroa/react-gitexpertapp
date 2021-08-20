802c047bc94147b3b7067ec840e271ac8eed3d18
josejulianfigueroa <josejulianfigueroa@gmail.com>
2021-08-20 16:20:46 -0300

    Docs creado

---
+++ docs/precache-manifest.aa25e20d0a5142467eb93b1ca4d60667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8df90f62a6aaa7c3209513bb24798d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c63aef6a83b1f011e70",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c63aef6a83b1f011e70",
+    "url": "/static/js/main.18c195dd.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
